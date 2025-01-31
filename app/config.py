b396ff81db6126c5b8f94938d00fed62db37bc1f
John Stoveld <jstoveld@gmail.com>
2025-01-31 08:33:14 -0600

    Timeout for session and JWT to 1h

--- app/config.py
+++ app/config.py
@@ -2,4 +2,4 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
